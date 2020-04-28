12482d52abcd74c65496eceb1445e16a46568e9f
Leo Pitt <leopitt@function-design.co.uk>
2020-04-28 21:13:55 +0100

    Various styling.

---
+++ docroot/src/components/Button/Button.js
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types"
+import React from "react"
+
+const Button = ({ label, className, disabled }) => (
+  <button
+    className={className}
+    type="submit"
+    disabled={disabled}
+  >
+    {label}
+  </button>
+)
+
+Button.propTypes = {
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  label: PropTypes.string,
+}
+
+Button.defaultProps = {
+  className: `c-button`,
+  disabled: false,
+  label: ``,
+}
+
+export default Button
--- docroot/src/components/NameForm/NameForm.js
+++ docroot/src/components/NameForm/NameForm.js
@@ -1,24 +1,60 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-import Button from "../button"
+import Button from "../Button/Button"
 
-const NameForm = ({ name }) => (
-  <form
-    method="post"
-    action="#"
-    class="c-nameform"
-  >
-    <p>My name is <input type="text" required="required" name="name" value={name} />, <Button label="find my song" />.</p>
-  </form>
-)
+export default class NameForm extends React.Component {
+  state = {
+    inputName: "",
+    isFormValid: false,
+  }
+
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+    if (value.length > 0) {
+      this.setState({
+        isFormValid: true,
+      })
+    } else {
+      this.setState({
+        isFormValid: false,
+      })
+    }
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    alert(`Welcome ${this.state.firstName} ${this.state.lastName}!`)
+  }
+
+  render() {
+    return <form
+      method="post"
+      class="c-nameform"
+     onSubmit={this.handleSubmit}
+    >
+      <p>My name is
+        <input
+          type="text"
+          required="required"
+          name="inputName"
+          value={this.state.inputName}
+          onChange={this.handleInputChange}
+        />,
+        <Button className="c-button c-button--big-text c-button--disabled" disabled={!this.state.isFormValid} label="find my song" />.</p>
+    </form>
+  }
+}
 
 NameForm.propTypes = {
-  name: PropTypes.string,
+  inputName: PropTypes.string,
 }
 
 NameForm.defaultProps = {
-  name: ``,
+  inputName: ``,
 }
-
-export default NameForm
