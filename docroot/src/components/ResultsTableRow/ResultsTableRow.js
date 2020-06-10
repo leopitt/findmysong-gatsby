72ba3bc48a0d0d768938e1c2b85689ae6e8a3a8e
Leo Pitt <leopitt@function-design.co.uk>
2020-06-10 11:21:48 +0100

    Don't show a spotify icon if there's no spotify id. Adjust size of form label.

--- docroot/src/components/ResultsTableRow/ResultsTableRow.js
+++ docroot/src/components/ResultsTableRow/ResultsTableRow.js
@@ -1,14 +1,30 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const ResultsTableRow = ({ title, artist, spotify_id, deezer }) => (
-  <tr className="c-results-table__row">
-    <td className="c-results-table__row__field">{title}</td>
-    <td className="c-results-table__row__field">{artist}</td>
-    <td className="c-results-table__row__field"><a href={"https://open.spotify.com/embed/track/" + spotify_id} target="_blank" rel="noopener noreferrer" className="c-results-table__row__spotify" title={"Play \"" + title + "\" on Spotify"}>Spotify</a></td>
-    <td className="c-results-table__row__field c-results-table__row__field--hidden">Deezer</td>
-  </tr>
-)
+export default class ResultsTableRow extends React.Component {
+  renderSpotifyIcon = ({ title, artist, spotify_id }) => {
+    if (spotify_id) {
+      return (
+        <a href={"https://open.spotify.com/embed/track/" + spotify_id} target="_blank" rel="noopener noreferrer" className="c-results-table__row__spotify" title={"Play \"" + title + "\" on Spotify"}>Spotify</a>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    const title = this.props.title
+    const artist = this.props.artist
+    const spotify_id = this.props.spotify_id
+
+    return <tr className="c-results-table__row">
+        <td className="c-results-table__row__field">{title}</td>
+        <td className="c-results-table__row__field">{artist}</td>
+        <td className="c-results-table__row__field">{this.renderSpotifyIcon({ title, artist, spotify_id })}</td>
+        <td className="c-results-table__row__field c-results-table__row__field--hidden">Deezer</td>
+      </tr>
+  }
+}
 
 ResultsTableRow.propTypes = {
   title: PropTypes.string,
@@ -23,5 +39,3 @@ ResultsTableRow.defaultProps = {
   deezer: ``,
   spotify_id: ``,
 }
-
-export default ResultsTableRow
--- docroot/src/pages/about.js
+++ docroot/src/pages/about.js
@@ -7,25 +7,25 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About" description="About Find My Song." />
     <h1 className="page__h1">About</h1>
-    <p>Developed as a Gatsby &amp; React learning exercise by <a href="https://github.com/leopitt" target="_blank">Leo Pitt</a>.</p>
+    <p>Developed as a Gatsby &amp; React learning exercise by <a href="https://github.com/leopitt">Leo Pitt</a>.</p>
     <h2 className="page__h2">Built with ...</h2>
     <ul>
-      <li><a href="https://github.com/margueriteroth/gatsby-prismic-starter-prist" target="_blank">Prismic Gatsby theme</a></li>
-      <li><a href="https://www.drupal.org/" target="_blank">Drupal 8</a></li>
-      <li><a href="https://www.gatsbyjs.org/" target="_blank">Gatsby</a></li>
-      <li><a href="https://www.musixmatch.com/" target="_blank">Musixmatch lyric API</a></li>
+      <li><a href="https://github.com/margueriteroth/gatsby-prismic-starter-prist">Prismic Gatsby theme</a></li>
+      <li><a href="https://www.drupal.org/">Drupal 8</a></li>
+      <li><a href="https://www.gatsbyjs.org/">Gatsby</a></li>
+      <li><a href="https://www.musixmatch.com/">Musixmatch lyric API</a></li>
     </ul>
     <h2 className="page__h2">Source code ...</h2>
     <ul>
-      <li><a href="https://github.com/leopitt/findmysong" target="_blank">Drupal 8 back-end</a></li>
-      <li><a href="https://github.com/leopitt/findmysong-gatsby" target="_blank">Gatsby front-end</a></li>
+      <li><a href="https://github.com/leopitt/findmysong">Drupal 8 back-end</a></li>
+      <li><a href="https://github.com/leopitt/findmysong-gatsby">Gatsby front-end</a></li>
     </ul>
     <h2 className="page__h2">Find me on ...</h2>
     <ul>
-      <li><a href="https://github.com/leopitt" target="_blank">Github</a></li>
-      <li><a href="https://www.drupal.org/u/leo-pitt" target="_blank">Drupal</a></li>
-      <li><a href="https://www.drupal.org/u/leo-pitt" target="_blank">Twitter</a></li>
-      <li><a href="https://www.linkedin.com/in/leo-pitt/" target="_blank">LinkedIn</a></li>
+      <li><a href="https://github.com/leopitt">Github</a></li>
+      <li><a href="https://www.drupal.org/u/leo-pitt">Drupal</a></li>
+      <li><a href="https://www.drupal.org/u/leo-pitt">Twitter</a></li>
+      <li><a href="https://www.linkedin.com/in/leo-pitt/">LinkedIn</a></li>
     </ul>
   </Layout>
 )
